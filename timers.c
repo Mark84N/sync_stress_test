f282d55b859c22e51e79d626bb808184be3db31b
dieiev.dmitry <dieiev.dima@gmail.com>
2018-10-16 21:57:42 +0300

    removed per-entry lock

--- timers.c
+++ timers.c
@@ -4,15 +4,16 @@
 #include <linux/interrupt.h>
 #include <linux/timer.h>
 #include <linux/slab.h>
+#include <linux/sched.h>
 #include <linux/delay.h>
 #include <linux/random.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("DMDE");
 
-#define INTERVAL_NEW_LEAVE 250
-#define INTERVAL_DEL_LEAVE 1500
-#define INTERVAL_EXPIRE_MINOR 20 * HZ
+#define INTERVAL_NEW_LEAVE 10
+#define INTERVAL_DEL_LEAVE 50
+#define INTERVAL_EXPIRE_MINOR 3 * HZ
 
 struct major {
 	struct list_head minors_head;
@@ -21,7 +22,7 @@ struct major {
 	int minors_count;
 	spinlock_t major_lock;
 	struct tasklet_struct cleanup_tasklet;
-	atomic_t cleanup;
+	volatile int cleanup;
 } *maj;
 
 struct minor {
@@ -29,7 +30,6 @@ struct minor {
 	struct list_head leaves_head;
 	struct timer_list timer;
 	struct major *maj;
-	spinlock_t minor_lock;
 	int leaves_count;
 	int is_alive;
 	int id;
@@ -86,7 +86,7 @@ static void del_leave(struct minor *min, int id)
 static void del_all_leaves(struct minor *min)
 {
 	struct leave *l, *tmp;
-
+	pr_crit("%s()\n", __func__);
 	list_for_each_entry_safe(l, tmp, &min->leaves_head, list) {
 			list_del(&l->list);
 			kfree(l);
@@ -108,46 +108,46 @@ static void tasklet_cleanup_handler(unsigned long arg)
 {
 	struct major *maj = (struct major *)arg;
 	struct minor *ptr, *tmp;
+	pr_crit("%s()\n", __func__);
 
-	spin_lock_bh(&maj->major_lock);
-	list_for_each_entry_safe(ptr, tmp, &maj->minors_head, list) {
 retry:
-		spin_lock_bh(&ptr->minor_lock);
+	spin_lock(&maj->major_lock);
+	list_for_each_entry_safe(ptr, tmp, &maj->minors_head, list) {
 		if (ptr->is_alive == 0) {
 			if (-1 == try_to_del_timer_sync(&ptr->timer)) {
 				pr_crit("%s %d: timer handler for min %d is running, will allow it to finish\n", 
 							__func__, __LINE__, ptr->id);
-				spin_unlock_bh(&ptr->minor_lock);
+				spin_unlock(&maj->major_lock);
 				goto retry;
 			}
 
 			list_del(&ptr->list);
 			del_all_leaves(ptr);
-			spin_unlock_bh(&ptr->minor_lock);
 			kfree(ptr);
 			continue;
 		}
-		spin_unlock_bh(&ptr->minor_lock);
 	}
-	spin_unlock_bh(&maj->major_lock);
-	atomic_set(&maj->cleanup, 0);
+	maj->cleanup = 0;
+	spin_unlock(&maj->major_lock);
 }
 
 static void minor_expire_timer_function(unsigned long arg)
 {
 	struct minor *min = (struct minor *)arg;
-	
-	spin_lock_bh(&min->minor_lock);
-	if (!timer_pending(&min->timer) && !atomic_read(&min->maj->cleanup)) {
+	pr_crit("%s()\n", __func__);
+
+	spin_lock(&min->maj->major_lock);
+	if (!timer_pending(&min->timer) && !maj->cleanup) {
 		min->is_alive = 0;
 		tasklet_schedule(&min->maj->cleanup_tasklet);
 	}
-	spin_unlock_bh(&min->minor_lock);
+	spin_unlock(&min->maj->major_lock);
 }
 
 static struct minor *init_min(struct major *maj, int id)
 {
 	struct minor *min = NULL;
+	pr_crit("%s()\n", __func__);
 
 	min = (struct minor *)kzalloc(sizeof(*min), GFP_ATOMIC);
 	if (!min) {
@@ -157,7 +157,6 @@ static struct minor *init_min(struct major *maj, int id)
 
 	INIT_LIST_HEAD(&min->list);
 	INIT_LIST_HEAD(&min->leaves_head);
-	spin_lock_init(&min->minor_lock);
 	list_add(&min->list, &maj->minors_head);
 	min->is_alive = 1;
 	min->id = id;
@@ -180,27 +179,26 @@ static void major_add_leave_handler(unsigned long arg)
 	leave_id = get_rand_id(10);
 	minor_id = leave_id / 2;
 
-	if (atomic_read(&maj->cleanup))
+	spin_lock(&maj->major_lock);
+	if (maj->cleanup) {
+		pr_crit("%s %d: return because of cleanup\n", __func__, __LINE__);
+		spin_unlock(&maj->major_lock);
 		return;
+	}
 
-	spin_lock_bh(&maj->major_lock);
 	min = find_minor_by_id(maj, minor_id);
 	if (min) {
-		spin_lock_bh(&min->minor_lock);
 		mod_timer(&min->timer, jiffies + INTERVAL_EXPIRE_MINOR);
 		min->is_alive = 1;
-		spin_unlock_bh(&min->minor_lock);
 	} else {
 		min = init_min(maj, minor_id);
 	}
 
 	if (min) {
-		spin_lock_bh(&min->minor_lock);
 		add_leave(min, leave_id);
-		spin_unlock_bh(&min->minor_lock);
 	}
 	mod_timer(&maj->add_timer, jiffies + INTERVAL_NEW_LEAVE);
-	spin_unlock_bh(&maj->major_lock);
+	spin_unlock(&maj->major_lock);
 }
 
 static void major_remove_leave_handler(unsigned long arg)
@@ -209,24 +207,23 @@ static void major_remove_leave_handler(unsigned long arg)
 	struct minor *min;
 	int leave_id, minor_id;
 
-	if (atomic_read(&maj->cleanup)) {
-		pr_crit("%s %d: return because of cleanup\n", __func__, __LINE__);
-		return;
-	}
-
 	leave_id = get_rand_id(10);
 	minor_id = leave_id / 2;
 
-	spin_lock_bh(&maj->major_lock);
+	spin_lock(&maj->major_lock);
+
+	if (maj->cleanup) {
+		pr_crit("%s %d: return because of cleanup\n", __func__, __LINE__);
+		spin_unlock(&maj->major_lock);
+		return;
+	}
 
 	min = find_minor_by_id(maj, minor_id);
 	if (min) {
-		spin_lock_bh(&min->minor_lock);
 		del_leave(min, leave_id);
-		spin_unlock_bh(&min->minor_lock);
 	}
 	mod_timer(&maj->remove_timer, jiffies + INTERVAL_DEL_LEAVE);
-	spin_unlock_bh(&maj->major_lock);
+	spin_unlock(&maj->major_lock);
 }
 
 int init_module(void)
@@ -250,7 +247,7 @@ int init_module(void)
 	maj->remove_timer.function = major_remove_leave_handler;
 	maj->remove_timer.data = (long unsigned int)maj;
 	add_timer(&maj->remove_timer);
-	atomic_set(&maj->cleanup, 0);
+	maj->cleanup = 0;
 
 	tasklet_init(&maj->cleanup_tasklet, tasklet_cleanup_handler, (long unsigned int)maj);
 
@@ -263,9 +260,7 @@ static void minor_mark_all_to_remove(struct major *maj)
 
 	spin_lock_bh(&maj->major_lock);
 	list_for_each_entry(min, &maj->minors_head, list) {
-		spin_lock_bh(&min->minor_lock);
 		min->is_alive = 0;
-		spin_unlock_bh(&min->minor_lock);
 	}
 	spin_unlock_bh(&maj->major_lock);
 	tasklet_schedule(&maj->cleanup_tasklet);
@@ -273,13 +268,15 @@ static void minor_mark_all_to_remove(struct major *maj)
 
 void cleanup_module(void)
 {
-	atomic_set(&maj->cleanup, 1);
+	maj->cleanup = 1;
 	del_timer_sync(&maj->add_timer);
 	del_timer_sync(&maj->remove_timer);
 	minor_mark_all_to_remove(maj);
 
-	while(1 == atomic_read(&maj->cleanup))
+	while(maj->cleanup) {
 		pr_crit("%s() looping to wait for removal completion\n", __func__);
-
+		schedule();
+	}
+	pr_crit("%s(): completed\n", __func__);
 	kfree(maj);
 }
